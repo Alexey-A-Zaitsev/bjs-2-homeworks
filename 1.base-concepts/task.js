eadf54baab60f82eae9eb66a4966d1b2cd3d899a
Alex Zaitsev <saizewalex@gmail.com>
2023-06-05 21:35:13 +0300

    ДЗ 1 Основные понятия JS

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,24 @@
 "use strict"
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	let arr = [];
+	var d = Math.pow(b, 2) - 4 * a * c; // Расчет дискриминанта
+	if (d < 0) {
+		return arr;
+	} else if (d == 0) {
+		arr.push((-b) / (2 * a));
+	} else if (d > 0) {
+		arr.push((-b + Math.sqrt(d)) / (2 * a));
+		arr.push((-b - Math.sqrt(d)) / (2 * a));
+	}
+	return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+	let convertedPercent = percent / 100 / 12; // преобразование процентной ставки в месячную
+	let loanBody = amount - contribution; // тело кредита
+	let monthlyFee = loanBody *
+		(convertedPercent + (convertedPercent / ((Math.pow((1 + convertedPercent), countMonths) - 1))))
+	let totalAmount = monthlyFee * countMonths;
+	return +totalAmount.toFixed(2);
 }
