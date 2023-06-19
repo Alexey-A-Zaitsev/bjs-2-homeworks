d4241d87315e38c89c61ec1c850e7d0de5d91881
Alex Zaitsev <saizewalex@gmail.com>
2023-06-19 16:28:21 +0300

    ДЗ 7 «Асинхронность»

---
+++ 7.async/task.js
@@ -0,0 +1,59 @@
+class AlarmClock {
+    constructor() {
+        this.alarmCollection = [];
+        this.intervalId = null;
+    }
+
+    addClock(time, callback) {
+        if(time === undefined || time === null || callback  === undefined) {
+            throw new Error('Отсутствуют обязательные аргументы');
+        }
+        if(this.alarmCollection.some(alarm => alarm.time === time)) {
+            console.warn('Уже присутствует звонок на это же время')
+        };
+
+        let alarm = {
+            callback: callback,
+            time: time,
+            canCall: true
+        };
+
+        this.alarmCollection.push(alarm);
+
+        
+    };
+
+    removeClock(time) {
+        this.alarmCollection = this.alarmCollection.filter(alarm => alarm.time !== time);
+    };
+    
+    getCurrentFormattedTime() {
+        return new Date().toLocaleTimeString().slice(0, -3);
+    };
+
+    start() {
+        if(!this.intervalId) {   
+            this.intervalId = setInterval(() => this.alarmCollection.forEach(alarm => {
+            if(alarm.time === this.getCurrentFormattedTime()  &&  alarm.canCall == true)  {
+                alarm.canCall = false;
+                alarm.callback();
+            }
+            }));
+        };
+    };
+      
+    stop()  {
+        clearInterval(this.intervalId);
+          this.intervalId = null;     
+    };
+  
+    resetAllCalls() {
+        this.alarmCollection.forEach(alarm => alarm.canCall = true);
+    };
+  
+    clearAlarms() {
+        this.stop();
+        this.alarmCollection = [];
+    };
+  
+}
